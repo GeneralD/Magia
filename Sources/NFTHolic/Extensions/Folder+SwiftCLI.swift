c6c71a1b8ef9b141fc7c1d5b1bc371cb360c3ede
GeneralD <yumejustice@gmail.com>
2022-07-03 22:57:31 +0900

    feat: create output dir if not exists

--- Sources/NFTHolic/Extensions/Folder+SwiftCLI.swift
+++ Sources/NFTHolic/Extensions/Folder+SwiftCLI.swift
@@ -1,8 +1,15 @@
 import Files
+import Foundation
 import SwiftCLI
 
 extension Folder: ConvertibleFromString {
 	public init?(input: String) {
+		let fileManager = FileManager.default
+		var isDir: ObjCBool = false
+		let dirExists = fileManager.fileExists(atPath: input, isDirectory: &isDir) && isDir.boolValue
+		if !dirExists {
+			try? fileManager.createDirectory(at: .init(fileURLWithPath: input), withIntermediateDirectories: true)
+		}
 		try? self.init(path: input)
 	}
 }

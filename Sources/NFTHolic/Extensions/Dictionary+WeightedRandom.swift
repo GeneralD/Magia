36bcd3614201c8a41d2107c98eec1f37034dbd26
GeneralD <yumejustice@gmail.com>
2022-07-18 00:54:31 +0900

    fix: small fix

--- Sources/NFTHolic/Extensions/Dictionary+WeightedRandom.swift
+++ Sources/NFTHolic/Extensions/Dictionary+WeightedRandom.swift
@@ -1,5 +1,6 @@
 extension Dictionary where Value == Double {
 	func weightedRandom() -> (element: Key, probability: Double)? {
+		guard values.reduce(0, +) > 0 else { return first.map { ($0.key, 0) }}
 		let dict = reduce(into: [Key: Range<Value>]()) { accum, pair in
 			let start = accum.values.map(\.upperBound).max ?? 0
 			accum[pair.key] = start..<(start + pair.value)

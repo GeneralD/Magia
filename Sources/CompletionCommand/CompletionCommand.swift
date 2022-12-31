04dc943136b8d5a7fa8572dbf190b9b3ea3a3054
GeneralD <yumejustice@gmail.com>
2022-12-31 13:50:54 +0900

    refactor: small changes

--- Sources/CompletionCommand/CompletionCommand.swift
+++ Sources/CompletionCommand/CompletionCommand.swift
@@ -3,7 +3,10 @@ import SwiftCLI
 public class CompletionCommand: Command {
 	public let name: String
 	public let shortDescription = "Generate zsh completions"
-	public let longDescription = "You should run this command by sending the output to a file named `_magia` on your $fpath"
+	public private(set) lazy var longDescription = """
+You should run this command by sending the output to a file named `_\(cli?.name ?? "")` on your $fpath.
+e.g. `\(cli?.name ?? "") \(name) > ~/.oh-my-zsh/completions/_\(cli?.name ?? "")`
+"""
 	private weak var cli: CLI?
 
 	public init(name: String, cli: CLI) {
